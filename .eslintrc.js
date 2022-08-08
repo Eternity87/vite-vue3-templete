9aabf7daf9ba8e829e372e468c083609a3ef1c13
JiFengXiang <874431334@qq.com>
2022-08-08 15:24:34 +0800

    build: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,52 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
   extends: [
     'plugin:vue/vue3-essential',
     'airbnb-base',
+    'plugin:import/recommended',
     'plugin:prettier/recommended' // 添加 prettier 插件
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
-    sourceType: 'module',
+    sourceType: 'module'
   },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-  ],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-  },
-};
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: [
+          'e', // for e.returnvalue
+          'ctx', // for Koa routing
+          'req', // for Express requests
+          'request', // for Express requests
+          'res', // for Express responses
+          'response', // for Express responses
+          'state' // for vuex state
+        ]
+      }
+    ],
+    // 'import/extensions': [
+    //   'error',
+    //   'ignorePackages',
+    //   {
+    //     js: 'never',
+    //     mjs: 'never',
+    //     jsx: 'never',
+    //     ts: 'never',
+    //     tsx: 'never'
+    //     // vue: 'never'
+    //   }
+    // ],
+    'no-console': 'off',
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'import/no-absolute-path': 'off',
+    'import/no-extraneous-dependencies': 'off'
+  }
+}
